1c218e49e2b66478e68dbbfea909991335f05285
simpart <simpart>
2018-10-14 13:56:38 +0000

    add comment

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mf.effect.Border = class extends mf.Effect {
         try {
             super();
             this.name('Border');
-            this.prmMap('width', 'color');
+            this.prmMap(['width', 'color']);
             this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
@@ -24,11 +24,19 @@ mf.effect.Border = class extends mf.Effect {
     enable () {}
     disable () {}
     
+    /**
+     * set/delete border config
+     * 
+     * @param flg (boolean) true : set border config
+     * @param flg (boolean) false : delete border config
+     * @param cmp (Component) mofron Component object
+     * @note private method
+     */
     contents (flg, cmp) {
         try {
             super.contents(flg, cmp);
             let set_style = {};
-            set_style[this.type() + '-width'] = (true === flg) ? this.width() : null;
+            set_style[this.type() + '-width'] = (true === flg) ? mf.func.getSize(this.width()).toString() : null;
             set_style[this.type() + '-style'] = (true === flg) ? this.style() : null;
             set_style[this.type() + '-color'] = (true === flg) ? this.color().toString() : null;
             cmp.style(set_style);
@@ -38,84 +46,106 @@ mf.effect.Border = class extends mf.Effect {
         }
     }
     
-    width (prm) {
+    /**
+     * setter/getter border width
+     * 
+     * @param val (string) css value of border width
+     * @param val (undefined) call as getter
+     * @return (string) css value of border width
+     */
+    width (val) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_width) ? '0.01rem' : this.m_width.toString();
-            }
-            /* setter */
-            this.m_width = mf.func.getSize(prm);
-            if (true === this.status()) {
+            let ret = this.member('width', 'string', val, '0.01rem');
+            if ( (undefined !== val)       &&
+                 (true === this.isExecd()) &&
+                 (true === this.status()) ) {
                 this.execute(true);
             }
+            return ret;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * setter/getter border style
+     *
+     * @param val (string) css value of border style
+     * @param val (undefined) call as getter
+     * @return (string) css value of border style
+     */
     style (prm) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_style) ? 'solid' : this.m_style;
-            }
-            /* setter */
-            if ('string' !== typeof prm) {
-                throw new Error('invalid parameter');
-            }
-            this.m_style = prm;
-            if (true === this.status()) {
+            let ret = this.member('style', 'string', prm, 'solid');
+            if ( (undefined !== prm)       && 
+                 (true === this.isExecd()) &&
+                 (true === this.status()) ) {
                 this.execute(true);
             }
+            return ret;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * setter/getter border type
+     * 
+     * @param prm (string) border target position
+     *            ('all', 'top', 'bottom', 'left', 'right', 'top-left', 'top-right', 'bottom-left', 'bottom-right')
+     * @param val (undefined) call as getter
+     * @return (string) css value of border type
+     */
     type (prm) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_type) ? 'border' : 'border-' + this.m_type;
+            let ret = this.member(
+                'type',
+                ['all', 'top', 'bottom', 'left', 'right', 'top-left', 'top-right', 'bottom-left', 'bottom-right'],
+                prm,
+                ''
+            );
+            if ( (undefined !== prm)       &&
+                 (true === this.isExecd()) && 
+                 (true === this.status()) ) {
+                this.execute(true);
+            } else if (undefined !== ret) {
+                return ('all' === ret) ? 'border' : 'border-' + ret;
             }
-            /* setter */
-            if ( ('top'    !== prm)   &&
-                 ('bottom' !== prm)   &&
-                 ('left'   !== prm)   &&
-                 ('right'  !== prm)   &&
-                 ('top-left'     !== prm) &&
-                 ('top-rigth'    !== prm) && 
-                 ('bottom-left'  !== prm) && 
-                 ('bottom-right' !== prm) ) {
-                throw new Error('invalid parameter');
-            }
-            this.m_type = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * setter/getter border color
+     * 
+     * @param prm (string) border color (color name, 'rgb(r,g,b)', '#HEX')
+     * @param prm (array) border color ([r,g,b])
+     * @param val (undefined) call as getter
+     * @return (string) css value of border type
+     */
     color (prm) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_color) ? new mf.Color(0,0,0).toString() : this.m_color.toString();
-            }
-            /* setter */
-            this.m_color = mf.func.getColor(prm);
-            if (true === this.status()) {
+            prm = (null === prm) ? prm = [0,0,0] : prm;
+            let ret = this.member(
+                'color',
+                'Color',
+                (undefined === prm) ? prm : mf.func.getColor(prm)
+            );
+            if ( (undefined !== prm)       &&
+                 (true === this.isExecd()) &&
+                 (true === this.status()) ) {
                 this.execute(true);
             }
+            return ret;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
-    
 }
 module.exports = mf.effect.Border;
 /* end of file */
